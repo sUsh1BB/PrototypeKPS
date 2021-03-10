841b7ab793471e4063cfac09aa64aa6b232cba4e
Sascha Brehmer <s.brehmer@gmx.net>
2021-03-10 18:46:40 +0100

    Revert "Revert "Map + Waypoints""
    This reverts commit 5c0ec150f96dddbbdd9f886520334b327c5c14aa.

--- Assets/Scripts/MinionMovement.cs
+++ Assets/Scripts/MinionMovement.cs
@@ -15,12 +15,8 @@ public class MinionMovement : MonoBehaviour
     public int maxWaypoints = 8;
     private int curWaypoint = 0;
     private int wpIndex;
-    //private int loopSafe;
+
     public float speed = 1, stopDistance = 1;
-
-
-
-
 
 
     // Start is called before the first frame update
@@ -35,7 +31,9 @@ public class MinionMovement : MonoBehaviour
         for(wpIndex=0; wpIndex < maxWaypoints; wpIndex++)
         {
             wp = GameObject.Find("WP" + curWaypoint);
+            //Debug
             Debug.Log("DIESE SCHLEIFE WIRD" + wpIndex + "MAL DURCHLAUFEN!");
+            //Debug
             WayPoints[curWaypoint] = wp.GetComponent<Transform>();
             Debug.Log(WayPoints[curWaypoint]);
             curWaypoint++;
